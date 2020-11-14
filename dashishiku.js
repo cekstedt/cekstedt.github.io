c112bef81072654e1a6e39dcae848e0fa21ae58e
cekstedt <9835700+cekstedt@users.noreply.github.com>
2020-11-13 21:18:48 -0800

    Add files via upload
    Initial commit 13.NOV.2020: 
     - index.html
     - dashishiku.js
     - Dictionary.js
     - data.json

---
+++ dashishiku.js
@@ -0,0 +1,28 @@
+// Initialize global variables.
+let dictionary;
+
+//import json data.
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myObj = JSON.parse(this.responseText);
+    }
+};
+xmlhttp.open("GET", "data.json", true);
+xmlhttp.send();
+
+xmlhttp.onload = function() {
+    dictionary = new Dictionary(myObj);
+    document.getElementById("button").innerHTML = "Generate!";
+};
+
+// Button function.
+function generate_haiku() {
+    if (document.getElementById("button").innerHTML == "Generate!") {
+        var haiku = [dictionary.parse_template(5),
+                     dictionary.parse_template(7),
+                     dictionary.parse_template(5)];
+
+        document.getElementById("text").innerHTML = haiku.join("<br>");
+    }
+}
